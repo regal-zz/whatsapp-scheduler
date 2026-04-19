7c27de59eb07bc08a733252cd0308d828b1ef603
regaldo <regaldo@gmail.com>
2026-04-19 14:51:35 -0600

    Update index.js Port 3000
    Agregar el Port 3000 para que escuche

--- index.js
+++ index.js
@@ -54,5 +54,6 @@ app.get('/groups', async (req, res) => {
   res.json(groups);
 });
 
-app.listen(3000, () => console.log('Servidor en puerto 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
 client.initialize();
